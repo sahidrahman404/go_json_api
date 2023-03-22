34de2b0d60b753c434dff178b3877fcacb57acb7
Sahid Rahman <sahidrahman404@gmail.com>
2023-03-22 20:30:41 +0700

    add json validator

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/sahidrahman404/go_json_api/internal/page/data"
+	"github.com/sahidrahman404/go_json_api/internal/validator"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -21,6 +22,21 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
 		return
 	}
+
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: data.Runtime(input.Runtime),
+		Genres:  input.Genres,
+	}
+
+	v := validator.New()
+
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
 	fmt.Fprintf(w, "%+v\n", input)
 }
 
